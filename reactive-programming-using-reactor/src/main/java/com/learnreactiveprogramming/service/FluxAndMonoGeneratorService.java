b2fc2fcbc0d73cd181d128ac79f4252f2d527f07
vg7venu <vg7venu@gmail.com>
2021-11-09 17:58:53 +0530

    My first mono funtion

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,6 +1,7 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -9,11 +10,19 @@ public class FluxAndMonoGeneratorService {
         return Flux.fromIterable(List.of("venue","seven","even"));
     }
 
+    public Mono<String> namesMono() {
+        return Mono.just("alex");
+    }
+
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
 
         System.out.println(fluxAndMonoGeneratorService.namesFlux().subscribe(name -> {
             System.out.println("I'm "+name);
         }));
+
+        fluxAndMonoGeneratorService.namesMono().subscribe(name -> {
+            System.out.println("This is mono "+name);
+        });
     }
 }
