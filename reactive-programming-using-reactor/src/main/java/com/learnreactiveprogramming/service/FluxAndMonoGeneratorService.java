818d8c4ce7d7d39590bafa1861d8870c81a33b84
vg7venu <vg7venu@gmail.com>
2021-11-09 17:56:15 +0530

    My first flux funtion

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,19 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("venue","seven","even"));
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+        System.out.println(fluxAndMonoGeneratorService.namesFlux().subscribe(name -> {
+            System.out.println("I'm "+name);
+        }));
+    }
 }
